96a90979edef02862e57fafc0f0853df7d10ac45
jimmyinn <zhangjimmy5@gmail.com>
2025-04-30 15:49:37 -0400

    Get rank function has been changed to use puuid as input. Removed getSumId as it's not necessary.

--- data/api.js
+++ data/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_KEY = process.env.RIOT_API_KEY;
+// Key needs to be renewed daily
+const API_KEY = "RGAPI-550f23c5-d8bd-42f1-ad70-58343f9ee48a";
 
 let region = "na1"
 let BASE_URL = `https://${region}.api.riotgames.com`  // Adjust region if needed
@@ -9,8 +10,9 @@ const BASE_URL_2 = "https://americas.api.riotgames.com";
 
 const getPuuid = async (summonerName, tagline, region) => {
     // Currently only have NA region, need to add other region codes
+    let regionUrl = region
     if (region == "NA") {
-        let regionUrl = "americas"
+        regionUrl = "americas"
     } 
     const headers = { "X-Riot-Token": API_KEY };
     const puuidUrl = `https://${regionUrl}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${summonerName}/${tagline}`;
@@ -31,32 +33,13 @@ const getPuuid = async (summonerName, tagline, region) => {
     }
 }
 
-const getSumId = async (puuid, region) => {
-    const headers = { "X-Riot-Token": API_KEY };
-    const sumIdUrl = `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}`;
-    try {
-        const response = await axios.get(sumIdUrl, { headers });
-        return response.data.id;
-    } catch (error) {
-        if (error.response) {
-            if (error.response.status === 403) {
-                throw "Error: Expired Riot API Key.";
-            } else if (error.response.status === 404) {
-                throw `Error: Link ${sumIdUrl} is wrong.`;
-            } else {
-                throw `Error: Riot API returned error code ${error.response.status}.`;
-            }
-        }
-        throw "Error: Failed to fetch summoner ID.";
-    }
-}
-
-const getRank = async (sumId, region) => {
+const getRank = async (puuid, region) => {
+    let regionUrl = region
     if (region == "NA") {
-        let regionUrl = "na1"
+        regionUrl = "na1"
     }
     const headers = { "X-Riot-Token": API_KEY };
-    const rankUrl = `https://${regionUrl}.api.riotgames.com/lol/league/v4/entries/by-summoner/${sumId}`;
+    const rankUrl = `https://${regionUrl}.api.riotgames.com/lol/league/v4/entries/by-puuid/${puuid}`;
     try {
         const response = await axios.get(rankUrl, { headers });
         if (!response.data) {
@@ -85,4 +68,6 @@ const getRank = async (sumId, region) => {
     }
 }
 
-export { getPuuid, getSumId, getRank };
+
+
+export { getPuuid, getRank };
