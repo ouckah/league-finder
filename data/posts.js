4067e2bf3a07d1a41293745d09af1065cb09b447
jimmyinn <zhangjimmy5@gmail.com>
2025-04-30 12:32:13 -0400

    Added input validation. Made image and tags optional.

--- data/posts.js
+++ data/posts.js
@@ -1,6 +1,6 @@
-import {posts} from '../config/mongoCollections.js';
-import {getUser} from './users.js';
-import {MongoNetworkTimeoutError, ObjectId} from 'mongodb';
+import { posts } from '../config/mongoCollections.js';
+import { getUser } from './users.js';
+import { MongoNetworkTimeoutError, ObjectId } from 'mongodb';
 import helpers from '../utils/helpers.js';
 
 const createPost = async (
@@ -10,20 +10,49 @@ const createPost = async (
     content,
     tags
 ) => {
-    
+    if (!userId) throw 'You must provide a userId';
+    if (!title) throw 'You must provide a title';
+    if (!content) throw 'You must provide content';
+
+    // Needs to be a real user
+    const user = await getUser(userId);
+
+    // image is optional.
+    if (image) {
+        if (typeof image !== 'string') throw 'image must be a string';
+        if (image.trim() === '') throw 'image cannot be an empty string';
+    }
+
+    // title is required.
+    if (typeof title !== 'string') throw 'title must be a string';
+    if (title.trim() === '') throw 'title cannot be an empty string';
+
+    // content is required.
+    if (typeof content !== 'string') throw 'content must be a string';
+    if (content.trim() === '') throw 'content cannot be an empty string';
+
+    // tags are optional.
+    if (tags) {
+        if (!Array.isArray(tags)) throw 'tags must be an array';
+        for (let i = 0; i < tags.length; i++) {
+            if (typeof tags[i] !== 'string') throw 'tags must be strings';
+            if (tags[i].trim() === '') throw 'tags cannot be empty strings';
+        }
+    }
+
     const post = {
-	userId: userId,
-	image: image,
-	title: title,
-	content: content,
-	tags: tags,
-	createdAt: new Date()
+        userId: userId,
+        image: image || '',
+        title: title,
+        content: content,
+        tags: tags || [],
+        createdAt: new Date().toUTCString()
     }
 
     const postsCollection = await posts();
     const insertInformation = await postsCollection.insertOne(post);
     if (!insertInformation) throw 'Could not add post';
-    
+
     return { postCreated: true };
 }
 
@@ -44,9 +73,7 @@ const getPost = async (
 const getUserPosts = async (
     userId
 ) => {
-    if (!userId) throw 'You must provide an id to search for';
-    if (typeof userId !== 'string') throw 'The id must be a string';
-    if (!ObjectId.isValid(userId)) throw 'The id is not a valid ObjectId';
+    const user = await getUser(userId);
 
     const postsCollection = await posts();
     const posts = await postsCollection.find({ userId: ObjectId(userId) }).toArray();
@@ -59,20 +86,19 @@ const editPost = async (
     postId,
     image,
     title,
-    content,    
+    content,
     tags
 ) => {
     if (!postId) throw 'You must provide an id to search for';
-    if (!image) throw 'You must provide an image';
     if (!title) throw 'You must provide a title';
     if (!content) throw 'You must provide content';
-    if (!tags) throw 'You must provide tags';
 
-    if (typeof postId !== 'string') throw 'The id must be a string';
-    if (!ObjectId.isValid(postId)) throw 'The id is not a valid ObjectId';
+    const post = await getPost(postId);
 
-    if (typeof image !== 'string') throw 'The image must be a string';
-    if (image.trim() === '') throw 'The image cannot be an empty string';
+    if (image) {
+        if (typeof image !== 'string') throw 'The image must be a string';
+        if (image.trim() === '') throw 'The image cannot be an empty string';
+    }
 
     if (typeof title !== 'string') throw 'The title must be a string';
     if (title.trim() === '') throw 'The title cannot be an empty string';
@@ -80,29 +106,31 @@ const editPost = async (
     if (typeof content !== 'string') throw 'The content must be a string';
     if (content.trim() === '') throw 'The content cannot be an empty string';
 
-    if (!Array.isArray(tags)) throw 'The tags must be an array';
-    for (let i = 0; i < tags.length; i++) {
-	if (typeof tags[i] !== 'string') throw 'The tags must be strings';
-	if (tags[i].trim() === '') throw 'The tags cannot be empty strings';
+    if (tags) {
+        if (!Array.isArray(tags)) throw 'The tags must be an array';
+        for (let i = 0; i < tags.length; i++) {
+            if (typeof tags[i] !== 'string') throw 'The tags must be strings';
+            if (tags[i].trim() === '') throw 'The tags cannot be empty strings';
+        }
     }
 
     try {
-	const post = await getPost(postId);
-	const postCollection = await posts();
-	const updatedPost = {
-	    image: image,
-	    title: title,
-	    content: content,
-	    tags: tags,
-	    createdAt: post.createdAt
-	};
-	await postCollection.updateOne(
-	    { _id: ObjectId(postId) },
-	    { $set: updatedPost }
-	);
-	return { postUpdated: true };
+        const post = await getPost(postId);
+        const postCollection = await posts();
+        const updatedPost = {
+            image: image || '',
+            title: title,
+            content: content,
+            tags: tags || [],
+            createdAt: post.createdAt
+        };
+        await postCollection.updateOne(
+            { _id: ObjectId(postId) },
+            { $set: updatedPost }
+        );
+        return { postUpdated: true };
     } catch (e) {
-	throw e;
+        throw e;
     }
 }
 
@@ -116,7 +144,7 @@ const deletePost = async (
     const postsCollection = await posts();
     const deletionInfo = await postsCollection.deleteOne({ _id: ObjectId(postId) });
     if (deletionInfo.deletedCount === 0) {
-	throw `Could not delete post with id of ${postId}`;
+        throw `Could not delete post with id of ${postId}`;
     }
 
     return { postDeleted: true };
