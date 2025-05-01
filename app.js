748e015d382c740c31481a576783bc5fb2fd490a
eddison <eddisonso@gmail.com>
2025-04-30 21:55:58 -0400

    bind to all interface instead of just localhost

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ setupMiddleware(app);
 configRoutes(app);
 
 const PORT = 3000
-const HOST = 'localhost'
+const HOST = '0.0.0.0'
 
 app.listen(PORT, HOST, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
