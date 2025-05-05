74bd74c35b05ac62f9957b99b6041160a7ebc001
eddison <eddisonso@gmail.com>
2025-05-05 01:57:18 -0400

    modified method call to use returned value directly

--- routes/teams.js
+++ routes/teams.js
@@ -31,14 +31,13 @@ router.route('/')
 	desiredRank = desiredRank.map(rank => rank.trim());
 	desiredRole = desiredRole.map(role => role.trim());
 
-	const result = await teamData.createTeam(
+	const id = await teamData.createTeam(
 	    title,
 	    desiredRank,
 	    desiredRole,
 	    region,
 	    description
 	);
-	const id = result.insertedId.toString();
 
 	res.redirect(`/teams/${id}`);
     })
