410916c0346d9d2e99d0d472f72206773b8cfc37
amanechibana <achibana@stevens.edu>
2025-05-08 00:11:29 -0400

    fixed up utils/validation.js

--- utils/validation.js
+++ utils/validation.js
@@ -1,37 +1,4 @@
-function checkString(strVal, varName) {
-    if (!strVal) throw `Error: You must supply a ${varName}!`;
-    if (typeof strVal !== 'string') throw `Error: ${varName} must be a string!`;
-    strVal = strVal.trim();
-    if (strVal.length === 0)
-	throw `Error: ${varName} cannot be an empty string or string with just spaces`;
-    if (!isNaN(strVal))
-	throw `Error: ${strVal} is not a valid value for ${varName} as it only contains digits`;
-    return strVal;
-}
-
-function checkStringArray(arr, varName) {
-    //We will allow an empty array for this,
-	//if it's not empty, we will make sure all tags are strings
-    if (!arr || !Array.isArray(arr))
-	throw `You must provide an array of ${varName}`;
-    for (let i in arr) {
-	if (typeof arr[i] !== 'string' || arr[i].trim().length === 0) {
-	    throw `One or more elements in ${varName} array is not a string or is an empty string`;
-	}
-	arr[i] = arr[i].trim();
-    }
-
-    return arr;
-}
-
-function checkStringWithLength(str, minLength, maxLength, chars) {
-    if (str.length < minLength || str.length > maxLength) {
-	throw new Error('String must be between ' + minLength + ' and ' + maxLength + ' characters long.');
-    }
-    if (!chars.test(str)) {
-	throw new Error('String contains illegal characters.');
-    }
-}
+import { checkString, checkStringArray, checkStringWithLength } from './helpers.js';
 
 const validateName = (name,varname) => {
     name = checkString(name, varname);
@@ -52,13 +19,12 @@ const validateUsername = (username,varname) => {
 };
 
 const validatePassword = (password, confirmPassword,varname) => {
-    checkString(password);
-    if (confirmPassword !== undefined && confirmPassword !== null) {
-	checkString(confirmPassword);
+    checkString(password,varname);
+	checkString(confirmPassword,varname);
 	if (password !== confirmPassword) {
 	    throw new Error('Passwords do not match');
 	}
-    }
+
     if (password.length < 8) {
 	throw new Error('Password must be at least 8 characters long');
     }
@@ -76,12 +42,11 @@ const validatePassword = (password, confirmPassword,varname) => {
 const validateRegistration = (
     firstName, lastName, email, username, password, confirmPassword
 ) => {
-
-    firstName = validateName(firstName);
-    lastName = validateName(lastName);
-    email = validateEmail(email);
-    username = validateUsername(username);
-    validatePassword(password, confirmPassword);
+    firstName = validateName(firstName,"firstName");
+    lastName = validateName(lastName,"lastName");
+    email = validateEmail(email,"email");
+    username = validateUsername(username,"username");
+    validatePassword(password, confirmPassword,"password");
 }
 
 const validateTeam = (
@@ -93,11 +58,11 @@ const validateTeam = (
     if (typeof desiredRole === 'string') {
 	desiredRole = [desiredRole];
     }
-    checkString(title);
+    checkString(title, 'title');
     checkStringArray(desiredRank, 'desiredRank');
     checkStringArray(desiredRole, 'desiredRole');
-    checkString(region);
-    checkString(description);
+    checkString(region, 'region');
+    checkString(description, 'description');
 }
 
 export { validateRegistration, validateTeam }
