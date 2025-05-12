4643132443385dc1c90da9cc37fe16b4857d8838
ouckah <aidan.ouckama@gmail.com>
2025-05-11 22:57:57 -0400

    function scaffolding

--- data/reputation.js
+++ data/reputation.js
@@ -1,5 +1,17 @@
-import {reputation} from '../config/mongoCollections.js';
+import {users} from '../config/mongoCollections.js';
 import {MongoNetworkTimeoutError, ObjectId} from 'mongodb';
 import helpers from '../utils/helpers.js';
 import * as validation from '../utils/validation.js';
 
+const upvote = async (userId) => {
+  const usersCollection = await users()
+}
+
+const downvote = async (userId) => {
+  const usersCollection = await users()
+}
+
+export {
+  upvote,
+  downvote,
+}
