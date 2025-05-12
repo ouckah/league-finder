730b9890ba50250d174e201d5699dd0f28d7ef16
ouckah <aidan.ouckama@gmail.com>
2025-05-11 22:55:33 -0400

    boilerplate for data functions for rep

--- data/index.js
+++ data/index.js
@@ -3,9 +3,11 @@ import userDataFunctions from './users.js';
 import commentDataFunctions from './comments.js';
 import friendDataFunctions from './friends.js';
 import teamDataFunctions from './teams.js';
+import reputationDataFunctions from './reputation.js'
 
 export const postData = postDataFunctions;
 export const userData = userDataFunctions;
 export const commentData = commentDataFunctions;
 export const friendData = friendDataFunctions;
 export const teamData = teamDataFunctions;
+export const reputationData = reputationDataFunctions;
---
+++ data/reputation.js
@@ -0,0 +1,5 @@
+import {reputation} from '../config/mongoCollections.js';
+import {MongoNetworkTimeoutError, ObjectId} from 'mongodb';
+import helpers from '../utils/helpers.js';
+import * as validation from '../utils/validation.js';
+
